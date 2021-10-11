27882f7dfb681a332f5f65a909dfcdf7f6274c3a
Julian <julian.a.p.c@hotmail.com>
2021-10-11 13:20:02 -0500

    java

--- src/main/java/GAntiLag.java
+++ src/main/java/GAntiLag.java
@@ -10,7 +10,7 @@ import java.util.TreeMap;
 @ExtensionInfo(
         Title = "GAntiLag",
         Description = "Allows you funny options",
-        Version = "1",
+        Version = "1.0",
         Author = "Julianty"
 )
 
